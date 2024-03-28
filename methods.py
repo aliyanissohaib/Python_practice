6b6a4a311691bcaad2a46d5cfc422f43f2277a63
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-28 06:15:10 +0500

    Update methods.py

---
+++ methods.py
@@ -0,0 +1,12 @@
+name = "Aliyan"
+
+print(len(name))
+print(name.find("y"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isalpha())
+print(name.isdigit())
+print(name.count("a"))
+print(name.replace("a","o"))
+print(name*3)
