0868017062446a770d331c187c5e491dc85a9fe8
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-29 19:44:59 +0500

    Update stringslicing.py

--- stringslicing.py
+++ stringslicing.py
@@ -5,6 +5,10 @@ name = "Aliyan Sohaib"
 
 first_name = name[:7]
 last_name = name[7:14]
+step_name = name[::3]
+reverse_name = name[::-1]
 
 print(first_name)
 print(last_name)
+print(step_name)
+print(reverse_name)
