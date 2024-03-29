b716c58eef7d2569c50816dfd2d715abbbcbc9e0
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-29 19:30:13 +0500

    Update mathfunc.py

--- mathfunc.py
+++ mathfunc.py
@@ -1,9 +1,16 @@
 import math
 
 pi = 3.14
+x= 2
+y = 3
+z = 4
+
+print(max(x,y,z))
+print(min(x,y,z))
 
 print(round(pi))
 print(math.ceil(pi))
 print(math.floor(pi))
 print(abs(pi))
 print(pow(pi,3))
+print(math.sqrt(pi))
