f369de5b86f0e4d206fae5078fe10dba49c38bc6
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-29 19:52:26 +0500

    Update stringslicing.py

--- stringslicing.py
+++ stringslicing.py
@@ -12,3 +12,12 @@ print(first_name)
 print(last_name)
 print(step_name)
 print(reverse_name)
+
+# slice(start, stop, step)
+
+web1 = "http://google.com"
+web2 = "http://facebook.com"
+
+slice = slice(7, -4)
+print(web1[slice])
+print(web2[slice])
