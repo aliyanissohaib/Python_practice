4a5615fbb72f5e23cc9470fe7828646d608d4bdf
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-30 19:56:25 +0500

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -1,10 +1,14 @@
+import time
+# 
+# for index in range(10):
+#     print(index)
+#
+# for i in range(50,100,3):
+#     print(i)
+# for r in "Aliyan":
+#     print(r)
 
-
-for index in range(10):
-    print(index)
-
-for i in range(50,100,3):
-    print(i)
-for r in "Aliyan":
-    print(r)
-
+for sec in range(10,0,-1):
+    print(sec)
+    time.sleep(1)
+print("Happy New Year")
