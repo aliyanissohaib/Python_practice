0aaf08e4f8e92edafdf5b8c8efbe4ea3bc0a91fe
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-30 16:59:49 +0500

    Update logicoperator.py

--- logicoperator.py
+++ logicoperator.py
@@ -1,1 +1,6 @@
-temp = int(input("what is the temperature outside"))
+temp = int(input("what is the temperature outside: "))
+
+if temp >= 0 and temp <= 30:
+    print("the temperature is good")
+elif temp < 0 and temp > 30:
+    print("the temperature is not good")
