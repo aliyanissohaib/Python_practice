9e21bcebf44a923b53452a47207e2daa9a279147
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-30 17:03:01 +0500

    Update logicoperator.py

--- logicoperator.py
+++ logicoperator.py
@@ -1,6 +1,11 @@
+# and ,or ,not
+
 temp = int(input("what is the temperature outside: "))
 
-if temp >= 0 and temp <= 30:
+if not(temp >= 0 and temp <= 30):
     print("the temperature is good")
-elif temp < 0 and temp > 30:
+elif temp < 0 or temp > 30:
     print("the temperature is not good")
+
+
+
