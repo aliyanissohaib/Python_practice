622499ca08a1650ae5e4e656efe33cb2f4d975ad
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-04-01 02:15:16 +0500

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -1,11 +1,11 @@
 import time
 #
 # for index in range(10):
-#     print(index)
-#
- for i in range(50,100,3):
+     print(index)
+
+for i in range(50,100,3):
      print(i)
- for r in "Aliyan":
+for r in "Aliyan":
      print(r)
 
 for sec in range(10,0,-1):
